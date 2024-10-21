e2258704b7390d6b8a6f2cdf2f481bc6746331b0
hmmohamed002 <mohamedalkhateb002>
2024-10-21 19:19:08 +0200

    Remove unnecessary files and folders

--- .cargo/registry/src/index.crates.io-6f17d22bba15001f/box_drawing-0.1.2/src/arc.rs
+++
@@ -1,4 +0,0 @@
-pub const DOWN_RIGHT:&str = "╭";
-pub const DOWN_LEFT:&str = "╮";
-pub const UP_LEFT:&str = "╯";
-pub const UP_RIGHT:&str = "╰";
--- .cargo/registry/src/index.crates.io-6f17d22bba15001f/railwayapp-3.15.3/src/controllers/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod database;
-pub mod deployment;
-pub mod environment;
-pub mod project;
-pub mod user;
-pub mod variables;
